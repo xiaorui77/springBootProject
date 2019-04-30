470f3d212faa9d936ed77fe9ad7901450512d0e2
yougtao <cytcyt123@163.com>
2019-05-01 02:01:03 +0800

    spring boot 测试项目

--- src/main/java/com/yongtao/xiaorui/SpringBootDemoApplication.java
+++ src/main/java/com/yongtao/xiaorui/SpringBootDemoApplication.java
@@ -8,6 +8,7 @@ public class SpringBootDemoApplication
 {
 
     public static void main(String[] args) {
+        System.out.println("bniaho ");
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 
