5dfaf0cecbeaf227c1c7417cd109dc2f4814014b
yougtao <cytcyt123@163.com>
2019-05-01 02:11:33 +0800

    这是一个测试spring boot

--- src/main/java/com/yongtao/xiaorui/SpringBootDemoApplication.java
+++ src/main/java/com/yongtao/xiaorui/SpringBootDemoApplication.java
@@ -9,6 +9,7 @@ public class SpringBootDemoApplication
 
     public static void main(String[] args) {
         System.out.println("bniaho");
+        System.out.println("hello");
         SpringApplication.run(SpringBootDemoApplication.class, args);
     }
 
